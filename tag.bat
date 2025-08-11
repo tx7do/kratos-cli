f8130c905b60e158b648ce70e806c5707e48581e
Bobo <yanglinbo@gmail.com>
2025-08-11 13:30:44 +0800

    feat: go mod update.

--- tag.bat
+++ tag.bat
@@ -1,7 +1,6 @@
-
 git tag config-exporter/v0.0.5 --force
 git tag sql-orm/v0.0.4 --force
 git tag sql-proto/v0.0.14 --force
-git tag sql-kratos/v0.0.2 --force
+git tag sql-kratos/v0.0.3 --force
 
 git push origin --tags
