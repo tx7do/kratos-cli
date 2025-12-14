8150135be1c7e2c890a60413e99c01590a591ab0
Bobo <yanglinbo@gmail.com>
2025-12-14 14:33:14 +0800

    feat: tag.

--- tag.bat
+++ tag.bat
@@ -2,5 +2,6 @@ git tag config-exporter/v0.0.5 --force
 git tag sql-orm/v0.0.4 --force
 git tag sql-proto/v0.0.14 --force
 git tag sql-kratos/v0.0.3 --force
+git tag gowind/v0.0.1 --force
 
 git push origin --tags
