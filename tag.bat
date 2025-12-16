2946cd538f057b9eef40254bab4edc2b1d5a3c66
Bobo <yanglinbo@gmail.com>
2025-12-16 15:57:30 +0800

    feat: go.mod.

--- tag.bat
+++ tag.bat
@@ -4,7 +4,7 @@ git tag config-exporter/v0.0.7 --force
 
 git tag sql-orm/v0.0.7 --force
 git tag sql-proto/v0.0.17 --force
-git tag sql-kratos/v0.0.5 --force
+git tag sql-kratos/v0.0.6 --force
 
 git tag gowind/v0.0.4 --force
 
