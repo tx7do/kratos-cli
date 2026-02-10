33d2a30e3eebd5c454040313be63f90ff1a1290a
Bobo <yanglinbo@gmail.com>
2026-02-10 18:23:03 +0800

    feat: update sql-orm to v0.0.10 and sql-proto to v0.0.22, and increment sql-kratos tag to v0.0.12

--- tag.bat
+++ tag.bat
@@ -4,7 +4,7 @@ git tag config-exporter/v0.0.8 --force
 
 git tag sql-orm/v0.0.10 --force
 git tag sql-proto/v0.0.22 --force
-git tag sql-kratos/v0.0.11 --force
+git tag sql-kratos/v0.0.12 --force
 
 git tag gowind/v0.0.11 --force
 
