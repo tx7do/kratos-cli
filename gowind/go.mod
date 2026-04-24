6804bef9d935dcfac5c5d8c53b5e1e1c57e54248
Bobo <yanglinbo@gmail.com>
2026-04-24 22:49:22 +0800

    feat: update service name generation path and refactor tests to use t.Context
    - Change service name constants output directory from pkg/service to pkg/serviceid and rename generated file to service_id.go
    - Update error messages and comments to reflect new paths
    - Refactor tests to use t.Context() instead of context.Background()

--- gowind/go.mod
+++ gowind/go.mod
@@ -9,7 +9,7 @@ require (
 	github.com/stretchr/testify v1.11.1
 	github.com/tx7do/go-utils v1.1.37
 	github.com/tx7do/go-utils/code_generator v0.0.1
-	github.com/tx7do/kratos-cli/generators v0.0.10
+	github.com/tx7do/kratos-cli/generators v0.0.11
 	golang.org/x/mod v0.35.0
 )
 
--- tag.bat
+++ tag.bat
@@ -1,4 +1,4 @@
-git tag generators/v0.0.10 --force
+git tag generators/v0.0.11 --force
 
 git tag config-exporter/v0.0.8 --force
 
@@ -6,6 +6,6 @@ git tag sql-orm/v0.0.13 --force
 git tag sql-proto/v0.0.25 --force
 git tag sql-kratos/v0.0.16 --force
 
-git tag gowind/v0.0.11 --force
+git tag gowind/v0.0.12 --force
 
 git push origin --tags
